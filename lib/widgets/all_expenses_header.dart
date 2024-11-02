e54a23ac063c5fcf19201d8b6771a167f272d63e
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-11-02 14:20:27 +0200

    Enhance AllExpensesHeader with styled title and monthly filter button

--- lib/widgets/all_expenses_header.dart
+++ lib/widgets/all_expenses_header.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/text_styles.dart';
 
 class AllExpensesHeader extends StatelessWidget {
   const AllExpensesHeader({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       children: [
-        
+        Text("All Expenses", style: AppStyles.styleSemiBold20(context)),
+        const Spacer(),
+        Container(
+          padding: const EdgeInsets.all(12),
+          decoration: ShapeDecoration(
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              side: const BorderSide(
+                width: 1,
+                color: Color(0xffF1F1F1),
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: Row(
+            children: [
+              Text("Monthly", style: AppStyles.styleMedium16(context)),
+              const SizedBox(width: 18),
+              Transform.rotate(
+                angle: -1.5708,
+                child: const Icon(
+                  Icons.arrow_back_ios_new_outlined,
+                  color: Color(0xFF064061),
+                ),
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
