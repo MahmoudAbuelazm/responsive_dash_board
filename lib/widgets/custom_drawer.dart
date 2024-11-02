45f4ccd9d81251bae9ba1eb54df7c0a7f895ff6d
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-11-02 13:03:00 +0200

    Enhance CustomDrawer by adding InActiveDrawerItem widgets for improved navigation options

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/drawer_item_model.dart';
+import 'package:responsive_dash_board/widgets/in_active_drawer_item.dart';
 import 'package:responsive_dash_board/widgets/user_info_list_tile.dart';
 
 import '../section/drawer_items_list_view.dart';
@@ -7,13 +9,11 @@ import '../utils/app_images.dart';
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
 
- 
-
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child:  const Column(
+      child: const Column(
         children: [
           UserInfoListTile(
             image: Assets.imagesAvatar3,
@@ -24,6 +24,18 @@ class CustomDrawer extends StatelessWidget {
             height: 8,
           ),
           DrawerItemsListView(),
+          Expanded(child: SizedBox()),
+          InActiveDrawerItem(
+            item: DrawerItemModel(
+                title: "Settings System", icon: Assets.imagesSettings),
+          ),
+          InActiveDrawerItem(
+            item: DrawerItemModel(
+                title: "Log Out Account", icon: Assets.imagesLogout),
+          ),
+          SizedBox(
+            height: 48,
+          ),
         ],
       ),
     );
