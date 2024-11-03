fa883255afec78f1c7df94e840f90cb4d7c143a6
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-11-03 14:38:15 +0200

    Add AllExpensesItemsListView widget for displaying expense items

---
+++ lib/widgets/all_expenses_items_list_view.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/all_expenses_item_model.dart';
+import 'package:responsive_dash_board/utils/app_images.dart';
+import 'package:responsive_dash_board/widgets/all_expenses_item.dart';
+
+class AllExpensesItemsListView extends StatelessWidget {
+  const AllExpensesItemsListView({super.key});
+
+  static const items = [
+    AllExpensesItemModel(
+        image: Assets.imagesBalance,
+        title: "Balance",
+        price: r"$20,129",
+        date: "Jan 2024"),
+    AllExpensesItemModel(
+        image: Assets.imagesIncome,
+        title: "Income",
+        price: r"$20",
+        date: "Jan 2021"),
+    AllExpensesItemModel(
+        image: Assets.imagesExpenses,
+        title: "Expense",
+        price: r"$20",
+        date: "Jan 2021"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return AllExpensesItem(
+          item: items[index],
+        );
+      },
+    );
+  }
+}
