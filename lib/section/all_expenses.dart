af7a792904f3f473f5322cfac737b8e0b4891003
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-11-03 19:54:52 +0200

    Introduce CustomBackgroundContainer for consistent styling in AllExpenses

--- lib/section/all_expenses.dart
+++ lib/section/all_expenses.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
 import '../widgets/all_expenses_header.dart';
 import '../widgets/all_expenses_items_list_view.dart';
+import '../widgets/custom_background_container.dart';
 
 class AllExpenses extends StatelessWidget {
   const AllExpenses({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-      ),
-      child: const Column(
+    return const CustomBackgroundContainer(
+      
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           AllExpensesHeader(),
