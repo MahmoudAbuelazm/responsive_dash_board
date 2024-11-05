3c35693f60083b635c8ca1937e6d93cdbb474104
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-11-05 23:17:33 +0200

    Replace PageView with ExpandablePageView in MyCardsPageView; update dependencies in pubspec.yaml and pubspec.lock

--- lib/section/my_cards_page_view.dart
+++ lib/section/my_cards_page_view.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/my_card.dart';
+import 'package:expandable_page_view/expandable_page_view.dart';
 
 class MyCardsPageView extends StatelessWidget {
   const MyCardsPageView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return PageView.builder(
+    return ExpandablePageView(
       scrollDirection: Axis.horizontal,
-      itemCount: 3,
-      itemBuilder: (context, index) {
+      children: List.generate(3, (context) {
         return const MyCard();
-      },
+      }),
     );
   }
 }
