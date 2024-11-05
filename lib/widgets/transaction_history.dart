b2f36d395d2b20a45ef56d5d20c66bc3f335d379
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-11-05 23:58:18 +0200

    Add TransactionHeader widget for improved transaction history display in TransactionHistory

--- lib/widgets/transaction_history.dart
+++ lib/widgets/transaction_history.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/utils/text_styles.dart';
 
+import 'transaction_header.dart';
+
 class TransactionHistory extends StatelessWidget {
   const TransactionHistory({super.key});
 
   @override
   Widget build(BuildContext context) {
     return  Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-          Text("Transaction History", style: AppStyles.styleSemiBold20(context)),
-          Text("See all", style: AppStyles.styleMedium16(context).copyWith(
-            color: const Color(0xff4DB7F2)
-          )),
-        ],)
+        const TransactionHeader(),
+        const SizedBox(height: 20),
+        Text("13 April 2021", style: AppStyles.styleMedium16(context).copyWith(
+          color: const Color(0xffA1A4B2)
+        )),
+
       ],
     );
   }
