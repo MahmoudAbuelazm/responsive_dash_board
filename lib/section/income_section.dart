01f78e7b1523bb90dbf9607d63169fa4819a83cc
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-11-06 22:52:11 +0200

    Refactor IncomeSection to improve layout and extract IncomeSectionBody for better code organization

--- lib/section/income_section.dart
+++ lib/section/income_section.dart
@@ -1,35 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/custom_background_container.dart';
-import 'package:responsive_dash_board/widgets/income_details.dart';
 
 import '../widgets/all_expenses_header.dart';
-import '../widgets/income_chart.dart';
+import 'income_section_body.dart';
 
 class IncomeSection extends StatelessWidget {
   const IncomeSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CustomBackgroundContainer(
-      child: Column(
-        children: [
-          AllExpensesHeader(
-            title: "Income",
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                child: IncomeChart(),
-              ),
-              Expanded(
-                flex: 2,
-                child: IncomeDetails(),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
+    double width = MediaQuery.sizeOf(context).width;
+    return width >= 1200 && width < 1750
+        ? SizedBox()
+        : const CustomBackgroundContainer(
+            child: Column(
+              children: [
+                AllExpensesHeader(
+                  title: "Income",
+                ),
+                IncomeSectionBody()
+              ],
+            ),
+          );
   }
 }
---
+++ lib/section/income_section_body.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+
+import '../widgets/income_chart.dart';
+import '../widgets/income_details.dart';
+
+class IncomeSectionBody extends StatelessWidget {
+  const IncomeSectionBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+          child: IncomeChart(),
+        ),
+        Expanded(
+          flex: 2,
+          child: IncomeDetails(),
+        ),
+      ],
+    );
+  }
+}
