10fe1769f4abbe89344aac6d818a8dd9d316d0fa
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-11-07 21:46:27 +0200

    Add padding to DetailedIncomeChart in IncomeSectionBody and adjust title styles for pie chart sections

--- lib/widgets/detailed_income_chart.dart
+++ lib/widgets/detailed_income_chart.dart
@@ -33,26 +33,29 @@ class _DetailedIncomeChartState extends State<DetailedIncomeChart> {
         sectionsSpace: 0,
         sections: [
           PieChartSectionData(
+            titlePositionPercentageOffset: activeIndex == 0 ? 1.5 : null,
             title: activeIndex == 0 ? "Design Service" : "40%",
             radius: activeIndex == 0 ? 50 : 40,
             titleStyle: AppStyles.styleMedium16(context).copyWith(
-              color: Colors.white,
+              color: activeIndex == 0 ? null : Colors.white,
             ),
             value: 40,
             color: const Color(0xff208BC7),
           ),
           PieChartSectionData(
+            titlePositionPercentageOffset: activeIndex == 1 ? 1.5 : null,
             title: activeIndex == 1 ? "Design Product" : "25%",
             value: 25,
             titleStyle: AppStyles.styleMedium16(context).copyWith(
-              color: Colors.white,
+              color: activeIndex == 1 ? null : Colors.white,
             ),
             radius: activeIndex == 1 ? 50 : 40,
             color: const Color(0xff4Db7F2),
           ),
           PieChartSectionData(
+            titlePositionPercentageOffset: activeIndex == 2 ? 1.3 : null,
             titleStyle: AppStyles.styleMedium16(context).copyWith(
-              color: Colors.white,
+              color: activeIndex == 2 ? null : Colors.white,
             ),
             radius: activeIndex == 2 ? 50 : 40,
             title: activeIndex == 2 ? "Product Royality" : "20%",
@@ -60,8 +63,9 @@ class _DetailedIncomeChartState extends State<DetailedIncomeChart> {
             color: const Color(0xff064060),
           ),
           PieChartSectionData(
+            titlePositionPercentageOffset: activeIndex == 3 ? 1.5 : null,
             titleStyle: AppStyles.styleMedium16(context).copyWith(
-              color: Colors.white,
+              color: activeIndex == 3 ? null : Colors.white,
             ),
             radius: activeIndex == 3 ? 50 : 40,
             value: 15,
