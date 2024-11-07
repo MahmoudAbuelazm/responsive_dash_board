05d7f9cb4bb3a0a93a5d8adc839c4857cc27063e
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-11-07 21:49:23 +0200

    Move responsive size functions to SizeConfig for better organization and accessibility

--- lib/main.dart
+++ lib/main.dart
@@ -17,22 +17,3 @@ class ResponsiveDashBoard extends StatelessWidget {
     );
   }
 }
-
-double getResponsiveSize(BuildContext context, {required double fontSize}) {
-  double scaleFactor = getScaleFactor(context);
-  double responsiveFontSize = fontSize * scaleFactor;
-  double lowerLimit = fontSize * 0.8;
-  double upperLimit = fontSize * 1.2;
-
-  return responsiveFontSize.clamp(lowerLimit, upperLimit);
-}
-
-double getScaleFactor(BuildContext context) {
-  double width = MediaQuery.sizeOf(context).width;
-  if (width < 600) {
-    return width / 400;
-  } else if (width < 900) {
-    return width / 700;
-  }
-  return width / 1000;
-}
--- lib/utils/size_config.dart
+++ lib/utils/size_config.dart
@@ -1,5 +1,7 @@
 
 
+import 'package:flutter/material.dart';
+
 class SizeConfig {
   static double desktop = 1200;
   static double tablet = 800;
@@ -10,4 +12,25 @@ class SizeConfig {
   //   screenWidth = MediaQuery.sizeOf(context).width;
   //   screenHeight = MediaQuery.sizeOf(context).height;
   // }
+}
+
+
+
+double getResponsiveSize(BuildContext context, {required double fontSize}) {
+  double scaleFactor = getScaleFactor(context);
+  double responsiveFontSize = fontSize * scaleFactor;
+  double lowerLimit = fontSize * 0.8;
+  double upperLimit = fontSize * 1.2;
+
+  return responsiveFontSize.clamp(lowerLimit, upperLimit);
+}
+
+double getScaleFactor(BuildContext context) {
+  double width = MediaQuery.sizeOf(context).width;
+  if (width < 600) {
+    return width / 400;
+  } else if (width < 900) {
+    return width / 700;
+  }
+  return width / 1000;
 }
