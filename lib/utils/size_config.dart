862f41022eb99a59f16e5ec8c66359b298d4bcb3
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-11-07 22:07:20 +0200

    Refactor responsive size functions and integrate them into text styles for improved accessibility and consistency

--- lib/utils/size_config.dart
+++ lib/utils/size_config.dart
@@ -14,23 +14,3 @@ class SizeConfig {
   // }
 }
 
-
-
-double getResponsiveSize(BuildContext context, {required double fontSize}) {
-  double scaleFactor = getScaleFactor(context);
-  double responsiveFontSize = fontSize * scaleFactor;
-  double lowerLimit = fontSize * 0.8;
-  double upperLimit = fontSize * 1.2;
-
-  return responsiveFontSize.clamp(lowerLimit, upperLimit);
-}
-
-double getScaleFactor(BuildContext context) {
-  double width = MediaQuery.sizeOf(context).width;
-  if (width < 600) {
-    return width / 400;
-  } else if (width < 900) {
-    return width / 700;
-  }
-  return width / 1000;
-}
--- lib/utils/text_styles.dart
+++ lib/utils/text_styles.dart
@@ -1,12 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import 'size_config.dart';
 
 abstract class AppStyles {
   static TextStyle styleRegular16(context) {
     return GoogleFonts.montserrat(
       color: const Color(0xFF064060),
-      // fontSize: getResponsiveFontSize(context, fontSize: 16),
-
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
       fontWeight: FontWeight.w400,
     );
   }
@@ -14,8 +16,7 @@ abstract class AppStyles {
   static TextStyle styleBold16(BuildContext context) {
     return GoogleFonts.montserrat(
       color: const Color(0xFF4EB7F2),
-      // fontSize: getResponsiveFontSize(context, fontSize: 16),
-
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
       fontWeight: FontWeight.w700,
     );
   }
@@ -23,8 +24,7 @@ abstract class AppStyles {
   static TextStyle styleMedium16(BuildContext context) {
     return GoogleFonts.montserrat(
       color: const Color(0xFF064061),
-      // fontSize: getResponsiveFontSize(context, fontSize: 16),
-
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
       fontWeight: FontWeight.w500,
     );
   }
@@ -32,8 +32,7 @@ abstract class AppStyles {
   static TextStyle styleMedium20(BuildContext context) {
     return GoogleFonts.montserrat(
       color: const Color(0xFFFFFFFF),
-      // fontSize: getResponsiveFontSize(context, fontSize: 20),
-
+      fontSize: getResponsiveFontSize(context, fontSize: 20),
       fontWeight: FontWeight.w500,
     );
   }
@@ -41,8 +40,7 @@ abstract class AppStyles {
   static TextStyle styleSemiBold16(BuildContext context) {
     return GoogleFonts.montserrat(
       color: const Color(0xFF064061),
-      // fontSize: getResponsiveFontSize(context, fontSize: 16),
-
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
       fontWeight: FontWeight.w600,
     );
   }
@@ -50,8 +48,7 @@ abstract class AppStyles {
   static TextStyle styleSemiBold20(BuildContext context) {
     return GoogleFonts.montserrat(
       color: const Color(0xFF064061),
-      // fontSize: getResponsiveFontSize(context, fontSize: 20),
-
+      fontSize: getResponsiveFontSize(context, fontSize: 20),
       fontWeight: FontWeight.w600,
     );
   }
@@ -59,8 +56,7 @@ abstract class AppStyles {
   static TextStyle styleRegular12(BuildContext context) {
     return GoogleFonts.montserrat(
       color: const Color(0xFFAAAAAA),
-      // fontSize: getResponsiveFontSize(context, fontSize: 12),
-
+      fontSize: getResponsiveFontSize(context, fontSize: 12),
       fontWeight: FontWeight.w400,
     );
   }
@@ -68,8 +64,7 @@ abstract class AppStyles {
   static TextStyle styleSemiBold24(BuildContext context) {
     return GoogleFonts.montserrat(
       color: const Color(0xFF4EB7F2),
-      // fontSize: getResponsiveFontSize(context, fontSize: 24),
-
+      fontSize: getResponsiveFontSize(context, fontSize: 24),
       fontWeight: FontWeight.w600,
     );
   }
@@ -77,8 +72,7 @@ abstract class AppStyles {
   static TextStyle styleRegular14(BuildContext context) {
     return GoogleFonts.montserrat(
       color: const Color(0xFFAAAAAA),
-      // fontSize: getResponsiveFontSize(context, fontSize: 14),
-
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
       fontWeight: FontWeight.w400,
     );
   }
@@ -86,38 +80,30 @@ abstract class AppStyles {
   static TextStyle styleSemiBold18(BuildContext context) {
     return GoogleFonts.montserrat(
       color: const Color(0xFFFFFFFF),
-      // fontSize: getResponsiveFontSize(context, fontSize: 18),
-
+      fontSize: getResponsiveFontSize(context, fontSize: 18),
       fontWeight: FontWeight.w600,
     );
   }
 }
 
-// // sacleFactor
-// // responsive font size
-// // (min , max) fontsize
-// double getResponsiveFontSize(context, {required double fontSize}) {
-//   double scaleFactor = getScaleFactor(context);
-//   double responsiveFontSize = fontSize * scaleFactor;
+double getResponsiveFontSize({required double fontSize}) {
+  double scaleFactor = getScaleFactor();
+  double responsiveFontSize = fontSize * scaleFactor;
+  double lowerLimit = fontSize * 0.8;
+  double upperLimit = fontSize * 1.2;
 
-//   double lowerLimit = fontSize * .8;
-//   double upperLimit = fontSize * 1.2;
+  return responsiveFontSize.clamp(lowerLimit, upperLimit);
+}
 
-//   return responsiveFontSize.clamp(lowerLimit, upperLimit);
-// }
-
-// double getScaleFactor(context) {
-//   // var dispatcher = PlatformDispatcher.instance;
-//   // var physicalWidth = dispatcher.views.first.physicalSize.width;
-//   // var devicePixelRatio = dispatcher.views.first.devicePixelRatio;
-//   // double width = physicalWidth / devicePixelRatio;
-
-//   double width = MediaQuery.sizeOf(context).width;
-//   if (width < SizeConfig.tablet) {
-//     return width / 550;
-//   } else if (width < SizeConfig.desktop) {
-//     return width / 1000;
-//   } else {
-//     return width / 1920;
-//   }
-// }
+double getScaleFactor() {
+  var dispatcher = PlatformDispatcher.instance;
+  var physicalWidth = dispatcher.views.first.physicalSize.width;
+  var devicePixelRatio = dispatcher.views.first.devicePixelRatio;
+  var width = physicalWidth / devicePixelRatio;
+  if (width < SizeConfig.tablet) {
+    return width / 550;
+  } else if (width < SizeConfig.desktop) {
+    return width / 1000;
+  }
+  return width / 1500;
+}
