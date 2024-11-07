c900aa265c19c58ad2a69df568ad099a413cf03b
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-11-07 21:40:21 +0200

    Update DetailedIncomeChart to display dynamic titles for pie chart sections based on active index

--- lib/widgets/detailed_income_chart.dart
+++ lib/widgets/detailed_income_chart.dart
@@ -33,26 +33,39 @@ class _DetailedIncomeChartState extends State<DetailedIncomeChart> {
         sectionsSpace: 0,
         sections: [
           PieChartSectionData(
-            value: 40,
+            title: activeIndex == 0 ? "Design Service" : "40%",
             radius: activeIndex == 0 ? 50 : 40,
             titleStyle: AppStyles.styleMedium16(context).copyWith(
               color: Colors.white,
             ),
+            value: 40,
             color: const Color(0xff208BC7),
           ),
           PieChartSectionData(
+            title: activeIndex == 1 ? "Design Product" : "25%",
             value: 25,
+            titleStyle: AppStyles.styleMedium16(context).copyWith(
+              color: Colors.white,
+            ),
             radius: activeIndex == 1 ? 50 : 40,
             color: const Color(0xff4Db7F2),
           ),
           PieChartSectionData(
+            titleStyle: AppStyles.styleMedium16(context).copyWith(
+              color: Colors.white,
+            ),
             radius: activeIndex == 2 ? 50 : 40,
+            title: activeIndex == 2 ? "Product Royality" : "20%",
             value: 20,
             color: const Color(0xff064060),
           ),
           PieChartSectionData(
+            titleStyle: AppStyles.styleMedium16(context).copyWith(
+              color: Colors.white,
+            ),
             radius: activeIndex == 3 ? 50 : 40,
             value: 15,
+            title: activeIndex == 3 ? "Other" : "15%",
             color: const Color(0xffE2DECD),
           ),
         ]);
