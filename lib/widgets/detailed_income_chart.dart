c900aa2ba2b42cb4063f18eea51225e528a99eb5
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2024-11-07 21:34:12 +0200

    Enhance DetailedIncomeChart by adding title styles to pie chart sections for improved readability

--- lib/widgets/detailed_income_chart.dart
+++ lib/widgets/detailed_income_chart.dart
@@ -1,5 +1,6 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/text_styles.dart';
 
 class DetailedIncomeChart extends StatefulWidget {
   const DetailedIncomeChart({super.key});
@@ -32,27 +33,26 @@ class _DetailedIncomeChartState extends State<DetailedIncomeChart> {
         sectionsSpace: 0,
         sections: [
           PieChartSectionData(
-            showTitle: false,
             value: 40,
             radius: activeIndex == 0 ? 50 : 40,
+            titleStyle: AppStyles.styleMedium16(context).copyWith(
+              color: Colors.white,
+            ),
             color: const Color(0xff208BC7),
           ),
           PieChartSectionData(
             value: 25,
             radius: activeIndex == 1 ? 50 : 40,
-            showTitle: false,
             color: const Color(0xff4Db7F2),
           ),
           PieChartSectionData(
             radius: activeIndex == 2 ? 50 : 40,
             value: 20,
-            showTitle: false,
             color: const Color(0xff064060),
           ),
           PieChartSectionData(
             radius: activeIndex == 3 ? 50 : 40,
             value: 15,
-            showTitle: false,
             color: const Color(0xffE2DECD),
           ),
         ]);
